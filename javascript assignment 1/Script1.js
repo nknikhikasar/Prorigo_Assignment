7f041f0b492ddd312dcc41bf6d873becda2fed4d
Nikhil Kasar <71208460+nknikhikasar@users.noreply.github.com>
2021-11-22 18:16:10 +0530

    final update

--- javascript assignment 1/Script1.js
+++ javascript assignment 1/Script1.js
@@ -1,39 +1,47 @@
-function ClickMe() {
+var arr = [];
+window.onload =function ClickMe() {
     fetch('https://fakestoreapi.com/products')
         .then((response) => response.json()) 
         .then((jsonBody) => {
             buildProductsList(jsonBody);
-        });  
+        });
 }
 
 function buildProductsList(products) {
-    var img = document.getElementById("div");
+    let product1 = "";
     for (let product of products) {
-        var a = document.createElement("img");
-        let f = product.image;
-        let l = product.title;
-        text = f.textContent || f.innerText;
-        a.src = text;
-        a.width = 80;
-        a.height = 80;
-        a.alt = 'xyz';
-        document.write(product.id);
-        document.write("<br>");
-        document.body.appendChild(a);
-        document.write("<br>");
-        document.write(product.title);
-        document.write("<br>");
-        document.write(product.price);
-        document.write("<br>");
-        document.write(product.description);
-        document.write("<br>");
-        document.write(product.category);
-        document.write("<br>");
-    } 
-}
+        product1 += `<div class="Product2">
+        <div class = "check"><input type="checkbox" id="myCheck" value="${product.title }" name ="title" onclick="AddCartList()"></div>
+        <div class = "product-image"><img src="${product.image}" height="100" width="100"></div>
+        
+        <div>
+        <div class = "product-title"><h4>${(product.title)}</h4></div>
+        <div><p>${(product.description)}</p></div>
+        <div ><p>${product.category}</p></div>
+        </div>
+        <div class = "product-price"><p>$${product.price}</p></div>
+        
+    </div>`;
+        
+    }
+    document.getElementById("Product").innerHTML = product1;
     
-function showName() {
-    ClickMe();   
 }
+function AddCartList() {
+    var checkboxes = document.getElementsByName("title");
+    let uniqueChars = document.getElementById("cart");;
+    for (var i = 0; i < checkboxes.length; i++) {
+        var productContainer = document.createElement('div');
+        if (checkboxes[i].checked == true) {
+            arr.push(checkboxes[i].value);
+            uniqueChars = [...new Set(arr)];
+        }
+        else if (checkboxes[i].checked == false) {
+            arr.pop(checkboxes[i].value);
+        }
+
+    }
+    document.getElementById("cart").innerHTML = uniqueChars;
 
 
+}
